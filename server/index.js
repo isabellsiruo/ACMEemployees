8b1d487622bddabffa90bdcde47876e2893d9604
Isabell Ventouris <isv2107@columbia.edu>
2025-03-31 15:16:38 -0400

    Initial working version: backend + frontend fully connected

--- server/index.js
+++ server/index.js
@@ -1,9 +1,35 @@
-// imports here for express and pg
+const express = require('express');
+const pg = require('pg');
+const path = require('path');
 
-// static routes here (you only need these for deployment)
+const app = express();
 
-// app routes here
+const client = new pg.Client({
+  user: 'isabell',
+  host: 'localhost',
+  database: 'acme_hr_db',
+  password: 'lalakiwi',
+  port: 5432
+});
 
-// create your init function
+client.connect();
 
-// init function invocation
+app.use(express.json());
+
+app.get('/api/employees', async (req, res, next) => {
+  try {
+    const result = await client.query('SELECT * FROM employees');
+    res.json(result.rows);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
